3111e9a4fbb7799d6afa12a432e00f43c5039cb5
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-21 23:32:37 -0600

    Finish the 'save' command logic

--- multi_clipboard.py
+++ multi_clipboard.py
@@ -2,15 +2,35 @@ import sys
 import clipboard
 import json
 
+SAVED_DATA = "clipboard.json"
+
+def save_data(filepath, data):
+  with open(filepath, "w") as f:
+    json.dump(data, f)
+
+def load_data(filepath):
+  try:
+    with open(filepath, "f") as f:
+      data = json.load(f)
+      return data
+  except:
+    return {}
 
 if len(sys.argv) == 2:
   command = sys.argv[1]
+  data = load_data(SAVED_DATA)
 
   if command == "save":
-    print("save")
+    key = input("Enter a key: ")
+    data[key] = clipboard.paste()
+    save_data(SAVED_DATA, data)
+    print("Data saved.")
   elif command == "load":
     print("load")
   elif command == "list":
     print("list")
   else:
     print("Unknown command")
+
+else:
+  print("Please pass exactly one command.")
